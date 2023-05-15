bb949045ea287d63cd93d2e5641b4365dac6f923
George Koutanis <georgios.koutanis@appliedtechnology.se>
2023-05-15 01:24:36 +0100

    site v1 typo fix

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
                 <div className='cover'></div>
                 <h1 className="section-heading"><small>Developing</small> <br />DELIGHTFUL EXPERIENCES</h1>
                 <hr className="" />
-                <p className="large about-text">Hello, my name is George fullstack Javascript developer. I love astronomy, music and video games.</p>
+                <p className="large about-text">Hello, my name is George, a fullstack Javascript developer. I love astronomy, music and video games.</p>
                 <p className="large about-text">From a young age the world of computers and technology captivated me. The moment i put my first "Hello World" in an html file and opened it, I knew a whole new world of possibilities was in front of me.</p>
                 <p className="large about-text">I am passionate about software development and find myself constantly seeking new challenges and opportunities to learn and grow. I am excited about the prospect of collaborating with teams to develop cutting-edge solutions to real-world problems, and contributing to the success of organisations through my technical expertise and innovative ideas.</p>
                 <p className="large about-text">Overall, I am a highly motivated and dedicated individual with a strong work ethic, and I am committed to continuously improving my skills and knowledge as a software developer. I am excited about what the future holds and am ready to take on new challenges in the field of software development.</p>
